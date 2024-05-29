549cedbfd9896e4897bb21ebf7a0adaddec8533b
FaiqCh-456 <faiqchoudaryofficial@gmail.com>
2024-05-29 16:32:38 +0500

    edited version

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -21,7 +21,6 @@ class BST {
         size = 0;
     }
 
-
     public void insert(int key) {
         root = insertRec(root, key, 0);
     }
@@ -44,7 +43,6 @@ class BST {
         node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
         return node;
     }
-
 
     public void delete(int key) {
         root = deleteRec(root, key, 0);
@@ -86,6 +84,14 @@ class BST {
         return minv;
     }
 
+    private int maxValue(TreeNode1 node) {
+        int maxv = node.key;
+        while (node.right != null) {
+            maxv = node.right.key;
+            node = node.right;
+        }
+        return maxv;
+    }
 
     public boolean search(int key) {
         return searchRec(root, key, 0);
@@ -105,7 +111,6 @@ class BST {
         return key < node.key ? searchRec(node.left, key, level + 1) : searchRec(node.right, key, level + 1);
     }
 
-
     public int getHeight() {
         return getHeight(root);
     }
@@ -116,51 +121,82 @@ class BST {
         }
         return node.height;
     }
+
     public int getSize() {
         return size;
     }
 
-
-    public static void main(String[] args) {
-
-        runTests();
+    public int getMinValue() {
+        return root == null ? Integer.MIN_VALUE : minValue(root);
     }
 
-    private static void runTests() {
-        BST bst = new BST();
+    public int getMaxValue() {
+        return root == null ? Integer.MAX_VALUE : maxValue(root);
+    }
 
+    public boolean isBalanced() {
+        return isBalancedRec(root);
+    }
+
+    private boolean isBalancedRec(TreeNode1 node) {
+        if (node == null) {
+            return true;
+        }
+
+        int leftHeight = getHeight(node.left);
+        int rightHeight = getHeight(node.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return false;
+        }
+
+        return isBalancedRec(node.left) && isBalancedRec(node.right);
+    }
+
+    public static void main(String[] args) {
+        BST bst = new BST();
 
         int[] elements = {15, 10, 20, 8, 12, 16, 25};
         for (int el : elements) {
             bst.insert(el);
         }
 
-
-        System.out.println("Size: " + bst.getSize() + " ");
-        System.out.println("Height: " + bst.getHeight() + "");
+        System.out.println("Size: " + bst.getSize());
+        System.out.println("Height: " + bst.getHeight());
+        System.out.println("Minimum value: " + bst.getMinValue());
+        System.out.println("Maximum value: " + bst.getMaxValue());
+        System.out.println("Is balanced: " + bst.isBalanced());
 
         bst.search(10);
         bst.search(100);
 
-
         bst.delete(20);
         System.out.println("Delete 20:");
-        System.out.println("Size: " + bst.getSize() + " ");
-        System.out.println("Height: " + bst.getHeight() + "");
+        System.out.println("Size: " + bst.getSize());
+        System.out.println("Height: " + bst.getHeight());
+        System.out.println("Minimum value: " + bst.getMinValue());
+        System.out.println("Maximum value: " + bst.getMaxValue());
+        System.out.println("Is balanced: " + bst.isBalanced());
         bst.search(20);
 
         bst.delete(15);
         System.out.println("Delete 15:");
-        System.out.println("Size: " + bst.getSize() + " ");
-        System.out.println("Height: " + bst.getHeight() + " ");
+        System.out.println("Size: " + bst.getSize());
+        System.out.println("Height: " + bst.getHeight());
+        System.out.println("Minimum value: " + bst.getMinValue());
+        System.out.println("Maximum value: " + bst.getMaxValue());
+        System.out.println("Is balanced: " + bst.isBalanced());
         bst.search(15);
 
         bst.delete(8);
         System.out.println("Delete 8:");
-        System.out.println("Size: " + bst.getSize() + " ");
-        System.out.println("Height: " + bst.getHeight() + " ");
+        System.out.println("Size: " + bst.getSize());
+        System.out.println("Height: " + bst.getHeight());
+        System.out.println("Minimum value: " + bst.getMinValue());
+        System.out.println("Maximum value: " + bst.getMaxValue());
+        System.out.println("Is balanced: " + bst.isBalanced());
         bst.search(8);
-
-
     }
+
+
 }
